8565adc8ee57e0092bb164c80da467e724c1ecdd
Vinícius Müller <vinigm.nho@gmail.com>
2022-07-25 22:21:50 -0300

    feat: complete parser

---
+++ Robotic/src/App/Program.fs
@@ -0,0 +1,20 @@
+﻿
+open Library
+open System
+
+"/tmp/robots.txt"
+|> IO.File.ReadLines
+|> Robotic.parse
+|> printfn "%A"
+
+//Parser.init
+//|> Parser.userAgent("codanado")
+//|> Parser.userAgent("sapato")
+//|> Parser.allow("/")
+//|> Parser.disallow("/")
+//|> Parser.crawlDelay(25)
+//|> Parser.userAgent("foobar")
+//|> Parser.allow("/testing")
+//|> Parser.crawlDelay(100)
+//|> Parser.finish
+//|> printfn "%A"
--- Robotic/src/Library/Library.fs
+++ Robotic/src/Library/Library.fs
@@ -2,53 +2,94 @@
 
 open System
 
-module RuleGroup =
-    type T =
-        { sitemap: string option // TODO: I think sitemap can also be a list
-          crawlDelay: int option
-          allows: string list
-          disallows: string list }
+type RuleGroup =
+    { crawlDelay: int option
+      allows: string list // TODO: Check for allowance and use hashsets?
+      disallows: string list }
 
-    let init =
+    static member Default =
         { allows = List.empty
           disallows = List.empty
-          crawlDelay = None
-          sitemap = None }
+          crawlDelay = None }
 
-module ParsingToken =
-    type ParsingToken =
-        { groups: Map<string, RuleGroup.T>
-          currentAgents: string list
-          currentRuleGroup: RuleGroup.T
-          isNewRuleGroup: bool }
 
-    let private addUserAgent (token: ParsingToken) (userAgent: string) : ParsingToken =
+type ParsingToken =
+    { groups: Map<string, RuleGroup>
+      currentAgents: string list
+      currentRuleGroup: RuleGroup
+      isNewRuleGroup: bool
+      sitemaps: string list }
+
+    static member Default =
+        { sitemaps = List.Empty
+          groups = Map.empty
+          currentAgents = List.empty
+          currentRuleGroup = RuleGroup.Default
+          isNewRuleGroup = false }
+
+
+type Robots =
+    { groups: Map<string, RuleGroup>
+      sitemaps: string list }
+
+
+module Parser =
+    let private addToMap token =
+        fun acc agent -> Map.add agent token.currentRuleGroup acc
+
+    let private addCurrentAgentsToGroup token =
+        token.currentAgents
+        |> List.fold (addToMap token) token.groups
+
+    let userAgent userAgent token =
         match token.isNewRuleGroup with
-        | true -> token
-        // at the end of the rule group, set currentRuleGroup to each
-        // named agent in the current group in the groups map.
+        | true ->
+            let groups = addCurrentAgentsToGroup token
+
+            { token with
+                groups = groups
+                currentAgents = [ userAgent ]
+                isNewRuleGroup = false }
+
         | false ->
             let agents = userAgent :: token.currentAgents
             { token with currentAgents = agents }
 
-    let init =
-        { groups = Map.empty
-          currentAgents = List.empty
-          currentRuleGroup = RuleGroup.init
-          isNewRuleGroup = false }
+    let crawlDelay crawlDelay token =
+        let newRoleGroup = { token.currentRuleGroup with crawlDelay = Some crawlDelay }
 
-    let folder token line =
-        match line with
-        | Some (directive, right) ->
-            match directive with
-            | "user-agent" -> addUserAgent token right
-            | "crawl-delay" -> token
-            | "allow" -> token
-            | "disallow" -> token
-            | "sitemap" -> token
-            | _ -> token
-        | None -> token
+        { token with
+            currentRuleGroup = newRoleGroup
+            isNewRuleGroup = true }
 
+    let addSitemap right token =
+        { token with
+            sitemaps = right :: token.sitemaps
+            isNewRuleGroup = true }
+
+    let allow right token =
+        let lst = right :: token.currentRuleGroup.allows
+        let roleGroup = { token.currentRuleGroup with allows = lst }
+
+        { token with
+            currentRuleGroup = roleGroup
+            isNewRuleGroup = true }
+
+    let disallow right token =
+        let lst = right :: token.currentRuleGroup.disallows
+        let roleGroup = { token.currentRuleGroup with disallows = lst }
+
+        { token with
+            currentRuleGroup = roleGroup
+            isNewRuleGroup = true }
+
+    let finish token =
+        let groups = addCurrentAgentsToGroup token
+
+        { groups = groups
+          sitemaps = token.sitemaps }
+
+    let init = ParsingToken.Default
 
 module Robotic =
     let private notHash c = not (c.Equals("#"))
@@ -61,7 +102,23 @@ module Robotic =
         | [| cmd; value |] -> Some(cmd.ToLower(), stripComments (value.Trim()))
         | _ -> None
 
-    let parse (lines: string list) =
+    let private folder token line =
+        match line with
+        | Some (directive, right) ->
+            match directive with
+            | "user-agent" -> Parser.userAgent right token
+            | "crawl-delay" -> Parser.crawlDelay (int right) token
+            | "allow" -> Parser.allow right token
+            | "disallow" -> Parser.disallow right token
+            | "sitemap" -> Parser.addSitemap right token
+            | _ -> token
+        | None -> token
+
+
+    let parse (lines: string seq) =
         lines
-        |> List.map processLine
-        |> List.fold ParsingToken.folder ParsingToken.init
+        |> Seq.map processLine
+        |> Seq.fold folder ParsingToken.Default
+        |> Parser.finish
+
+// TODO: Add module to check for allowance
